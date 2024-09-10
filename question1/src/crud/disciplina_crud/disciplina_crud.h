149a9b69b171ca29e2d3c32938535ac40894b822
KauaHenSilva <kaua.sbc@gmail.com>
2024-09-10 01:08:14 -0300

    fix: add and mov functions .h

--- question1/src/crud/disciplina_crud/disciplina_crud.h
+++ question1/src/crud/disciplina_crud/disciplina_crud.h
@@ -6,6 +6,5 @@
 // - A carga horária deve ser múltiplo de 15, variando entre 30 e 90.
 void cadastrarDisciplinas();
 
-// - Permitir a remoção de uma disciplina da árvore de matrícula de um determinado aluno.
-void removerDisciplinaDaArvoreDeMatricula();
+
 
--- question1/src/crud/matricula_crud/matricula_crud.h
+++ question1/src/crud/matricula_crud/matricula_crud.h
@@ -4,3 +4,5 @@
 // - A matrícula deve ser organizada e conter somente um código de disciplina do curso do aluno.
 void cadastrarMatriculas();
 
+// - Permitir a remoção de uma disciplina da árvore de matrícula de um determinado aluno.
+void removerDisciplinaDaArvoreDeMatricula();
