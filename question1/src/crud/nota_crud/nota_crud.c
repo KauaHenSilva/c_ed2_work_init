50a65ac8720c8691ad751a8acb16ecbc11b32828
KauaHenSilva <kaua.sbc@gmail.com>
2024-09-14 17:17:40 -0300

    docs: update documentation for nota_crud functions, including enhancements to prencherNota and showAllNotas

--- question1/src/crud/nota_crud/nota_crud.c
+++ question1/src/crud/nota_crud/nota_crud.c
@@ -9,8 +9,8 @@
  * @brief Aloca memória para uma nova nota.
  *
  * Esta função aloca dinamicamente memória para um novo nó de nota (`NodeNota`),
- * inicializando seus campos com valores padrão (-1 para `codDisciplina`, 
- * `notaFinal` e `semestreCursado`) e definindo os ponteiros para subárvores 
+ * inicializando seus campos com valores padrão (-1 para `codDisciplina`,
+ * `notaFinal` e `semestreCursado`) e definindo os ponteiros para subárvores
  * esquerda e direita como `NULL`.
  *
  * @param node Ponteiro duplo para o nó de nota que será alocado.
@@ -68,11 +68,10 @@ void showNota(NodeNota *node)
   printf("\tCodDisciplina: %d\n", node->codDisciplina);
 }
 
-
 /**
  * @brief Exibe todas as notas de uma árvore binária.
  *
- * Função recursiva que percorre toda a árvore de notas e exibe os detalhes 
+ * Função recursiva que percorre toda a árvore de notas e exibe os detalhes
  * de cada nó.
  *
  * @param raiz Ponteiro para o nó raiz da árvore de notas.
@@ -88,10 +87,11 @@ void showAllNotas(NodeNota *raiz)
 }
 
 
+#if DEBUG_MODE
 /**
  * @brief Preenche os dados de uma nota com base na entrada do usuário.
  *
- * Solicita ao usuário o código da disciplina para preencher a nota. 
+ * Solicita ao usuário o código da disciplina para preencher a nota.
  * Se a entrada for inválida, a função retorna um erro.
  *
  * @param node Ponteiro para o nó de nota a ser preenchido.
@@ -109,14 +109,10 @@ int prencherNota(NodeNota *raizNota, NodeDisciplina *raizDisciplina)
   {
     eneunciado = "Digite o codigo da disciplina da nota: ";
 
-#if DEBUG_MODE
     if (raizNota->codDisciplina == -1)
       confirm = getInt(&raizNota->codDisciplina, eneunciado);
     else
       printf("[DEBUG]: procurando disciplinas com o id: %d\n", --raizNota->codDisciplina);
-#else
-    confirm = getInt(&raizNota->codDisciplina, eneunciado);
-#endif
 
     search_matricula(raizDisciplina, raizNota->codDisciplina, &search);
   } while (!search);
@@ -126,11 +122,43 @@ int prencherNota(NodeNota *raizNota, NodeDisciplina *raizDisciplina)
 
   return !confirm;
 }
+#else
+/**
+ * @brief Preenche os dados de uma nota com base na entrada do usuário.
+ *
+ * Solicita ao usuário o código da disciplina para preencher a nota.
+ * Se a entrada for inválida, a função retorna um erro.
+ *
+ * @param node Ponteiro para o nó de nota a ser preenchido.
+ * @return Retorna 1 se o preenchimento foi bem-sucedido, 0 caso contrário.
+ */
+int prencherNota(NodeNota *raizNota, NodeDisciplina *raizDisciplina)
+{
+  printf("Para sair só digite 'sair'.\n");
+
+  int confirm;
+  char *eneunciado;
+
+  NodeDisciplina *search = NULL;
+  do
+  {
+    eneunciado = "Digite o codigo da disciplina da nota: ";
+    confirm = getInt(&raizNota->codDisciplina, eneunciado);
+
+    search_matricula(raizDisciplina, raizNota->codDisciplina, &search);
+  } while (!search);
+
+  if (!confirm)
+    printf("Não foi possivel execultar o prencher a nota: ");
+
+  return !confirm;
+}
+#endif
 
 /**
  * @brief Insere um novo nó de nota na árvore binária.
  *
- * A função insere um novo nó de nota em uma árvore binária de acordo com o 
+ * A função insere um novo nó de nota em uma árvore binária de acordo com o
  * código da disciplina, preservando a ordenação.
  *
  * @param raiz Ponteiro para o ponteiro do nó raiz da árvore de notas.
