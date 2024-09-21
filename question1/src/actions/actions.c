fa20ece76eadd4e295459da43638ac3904317a0c
rayss4lves <rayssasantos.com890@gmail.com>
2024-09-21 10:42:06 -0300

    ...

--- question1/src/actions/actions.c
+++ question1/src/actions/actions.c
@@ -3,12 +3,3 @@
 
 #include "../crud/aluno_crud/aluno_crud.h"
 
-void mostrarTodosOsAlunosDeUmCurso(NodeCurso *curso, ListAluno *alunos)
-{
-  if (alunos)
-  {
-    if (curso->curso.codigo == alunos->aluno.codigoDoCurso)
-      showAluno(alunos);
-    mostrarTodosOsAlunosDeUmCurso(curso, alunos->prox);
-  }
-}
