e8d68dfe1b11cff4289fd7f699bebcd4e728ee05
Kaua Hen <115280997+KauaHenSilva@users.noreply.github.com>
2024-10-15 12:14:56 -0300

    Delete FinalizandoArvoreAVL directory

--- FinalizandoArvoreAVL/src/structs/info/info.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "struct_curso.h"
-#include "struct_disciplina.h"
-#include "struct_nota.h"
-#include "struct_matricula.h"
--- FinalizandoArvoreAVL/src/utils/tempos/tempoInsercaoCurso/tempos_insercao_curso.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define QTDCURSOTESTADOS 1000
-
-void testTempoCurso();
