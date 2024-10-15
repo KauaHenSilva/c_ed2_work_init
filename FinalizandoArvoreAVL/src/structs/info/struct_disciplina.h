9393891b6985885c1159eb1efdc595f5e77a1b14
Kaua Hen <115280997+KauaHenSilva@users.noreply.github.com>
2024-10-15 12:14:10 -0300

    Delete FinalizandoArvoreAVL directory

--- FinalizandoArvoreAVL/src/structs/info/struct_disciplina.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct stDisciplina
-{
-  int codigo;
-  char *nomeDaDisciplina;
-  int periodo;
-  int cargaHoraria;
-} Disciplina;
--- FinalizandoArvoreAVL/src/utils/menu/menu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-#include "../../structs/info/struct_curso.h"
-#include "../../structs/struct_aluno.h"
-
-void menu(int *opcao);
-void initMenu(ListAluno *alunos, Arvore *arvoreCursos);
